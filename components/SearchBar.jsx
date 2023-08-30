df5f8fd990b055fdb2e995bc34f1649b10db78f8
ovie009 <komitexstock@gmail.com>
2023-08-30 14:19:27 +0100

    Add filter buttons to search bar

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -2,6 +2,7 @@
 import { View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 // icons
 import SearchIcon from "../assets/icons/SearchIcon";
+import FilterIcon from "../assets/icons/FilterIcon";
 import ClearSearch from "../assets/icons/ClearSearch";
 // react hooks
 import { useState } from "react";
@@ -17,46 +18,59 @@ const SearchBar = ({placeholder, searchQuery, setSearchQuery, backgroundColor}) 
 
     // render SearchBar component
     return (
-        <View style={[style.searchBar, {backgroundColor: backgroundColor}]}>
-            {/* search icon */}
-            <SearchIcon />
-            {/* search bar text input */}
-            <TextInput 
-                style={style.searchInput}
-                placeholder={placeholder}
-                inputMode="search"
-                value={searchQuery}
-                onChangeText={handleSearchQuery}
-                placeholderTextColor={"#837f7f"}
-            />
-            {/* clear search button, only visible when search bar isn't empty */}
-            { searchQuery !== '' && (
-                <TouchableOpacity
-                    style={style.clearSearch}
-                    onPress={() => setSearchQuery("")}
-                >
-                    <ClearSearch />
-                </TouchableOpacity>
-            )}
+        <View style={style.searchBarWrapper}>
+            <View style={[style.searchBar, {backgroundColor: backgroundColor}]}>
+                {/* search icon */}
+                <SearchIcon />
+                {/* search bar text input */}
+                <TextInput 
+                    style={style.searchInput}
+                    placeholder={placeholder}
+                    inputMode="search"
+                    value={searchQuery}
+                    onChangeText={handleSearchQuery}
+                    placeholderTextColor={"#837f7f"}
+                />
+                {/* clear search button, only visible when search bar isn't empty */}
+                { searchQuery !== '' && (
+                    <TouchableOpacity
+                        style={style.clearSearch}
+                        onPress={() => setSearchQuery("")}
+                    >
+                        <ClearSearch />
+                    </TouchableOpacity>
+                )}
+            </View>
+            <TouchableOpacity style={[style.filterButton, {backgroundColor: backgroundColor}]}>
+                <FilterIcon />
+            </TouchableOpacity>
         </View>
     );
 }
 
 // stylesheet
 const style = StyleSheet.create({
+    searchBarWrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        gap: 12,
+        width: "100%",
+        height: 40,
+        marginBottom: 20
+    },
     searchBar: {
+        height: '100%',
+        flex: 1,
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        width: "100%",
-        height: 40,
         paddingHorizontal: 12,
         paddingVertical: 10,
         gap: 10,
         borderRadius: 12,
         backgroundColor: "#f8f8f8",
-        marginBottom: 20
     },
     searchInput: {
         fontFamily: "mulish-semibold",
@@ -64,6 +78,16 @@ const style = StyleSheet.create({
         flex: 1,
         color: black,
     },
+    filterButton: {
+        backgroundColor: "#f8f8f8",
+        width: 40,
+        height: 40,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 12,
+    }
 })
  
 export default SearchBar;
