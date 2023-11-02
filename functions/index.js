3d4fc07d9d28939bbbbc2254eb895651e79a229d
ovie009 <komitexstock@gmail.com>
2023-11-02 10:02:07 +0100

    v2 oncall function

--- functions/index.js
+++ functions/index.js
@@ -1,48 +1,55 @@
-const functions = require("firebase-functions");
+const {onCall} = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
 
 // function to update role
-exports.setRole = functions.https.onCall(async (data, context) => {
-  return admin.auth().getUserByEmail(data.email).then(async (user) => {
+exports.setRole = onCall(async (request) => {
+  return admin.auth().getUserByEmail(request.data.email).then(async (user) => {
     // if user is not logged in
-    if (context.auth.uid === null) {
-      throw new Error("Not authorized");
+    if (request.auth.uid === null) {
+      throw new Error("No auth data present");
     }
+    console.log("Write user claims: ", user.customClaims);
     // if role token has not been set
     if (user.customClaims.role === undefined) {
+      console.log("New User");
       // Check if there are other users with the same business_id
       const usersSnapshot = await admin.firestore().collection("users")
-          .where("business_id", "==", data.business_id)
+          .where("business_id", "==", request.data.business_id)
           .get();
       if (usersSnapshot.size > 1) {
         // users with same business_id exist, so user can't be adminu
         return admin.auth().setCustomUserClaims(user.uid, {
           admin: false,
-          role: data.role,
-          account_type: data.account_type,
+          role: request.data.role,
+          account_type: request.data.account_type,
           deactivated: false,
         });
       } else {
         // This is the first user with the business_id, so they can be admin
         return admin.auth().setCustomUserClaims(user.uid, {
           admin: true,
-          role: data.role,
-          account_type: data.account_type,
+          role: request.data.role,
+          account_type: request.data.account_type,
           deactivated: false,
         });
       }
     }
+    console.log("Auth user claims: ", request.auth.token);
     // if user is not a manager throw error
-    if (user.customClaims.role !== "Manager") {
-      throw new Error("Not authorized");
+    if (request.auth.token.role !== "Manager") {
+      throw new Error("Only managers can edit roles");
+    }
+    // if user is not a manager throw error
+    if (user.customClaims.admin) {
+      throw new Error("Cannot edit admin");
     }
     // set token
     return admin.auth().setCustomUserClaims(user.uid, {
       admin: false,
-      role: data.role,
-      account_type: data.account_type,
+      role: request.data.role,
+      account_type: request.data.account_type,
       deactivated: false,
     });
   }).then(() => {
